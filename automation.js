f79c9ba2b748c1fadc4afe612987d60c2bc13080
Timothy Herchen <timothy.herchen@gmail.com>
2018-12-27 16:47:15 -0800

    linearautomationsegment

--- automation.js
+++ automation.js
@@ -17,4 +17,72 @@ class AutomationSegment {
     deltaY() {
         return this.y2 - this.y1;
     }
+
+    _timeIntegralCheck() {
+        if (this.ymin && this.ymin() <= 0)
+            throw new Error("Time integral does not exist, because the y minimum is less than 0.");
+    }
+}
+
+const linearAutomationTimeIntegralEps = 1e-9;
+
+// Note: this.derivativeAt(0) is just an easy way to get the slope
+class LinearAutomationSegment extends AutomationSegment {
+    constructor(...args) {
+        super(...args);
+    }
+
+    ymin() {
+        return Math.min(this.y1, this.y2);
+    }
+
+    ymax() {
+        return Math.max(this.y1, this.y2);
+    }
+
+    valueAt(x) {
+        return (x - this.x1) * this.derivativeAt() + this.y1;
+    }
+
+    derivativeAt(x) {
+        return (this.y2 - this.y1) / (this.x2 - this.x1);
+    }
+
+    integralAt(x) {
+        let xd = x - this.x1;
+        return this.y1 * xd + this.derivativeAt() * xd * xd / 2;
+    }
+
+    timeIntegral(x) {
+        let m = this.derivativeAt();
+        if (Math.abs(m) < linearAutomationTimeIntegralEps) {
+            return (x - this.x1) / this.y1;
+        }
+
+        return (Math.log(m * (x - this.x1) + this.y1) - Math.log(this.y1)) / m;
+    }
+
+    translateX(x) {
+        this.x1 += x;
+        this.x2 += x;
+    }
+
+    translateY(y) {
+        this.y1 += y;
+        this.y2 += y;
+    }
+
+    scaleX(x) {
+        this.x1 *= x;
+        this.x2 *= x;
+    }
+
+    scaleY(y) {
+        this.y1 *= y;
+        this.y2 *= y;
+    }
+
+    clone() {
+        return new LinearAutomationSegment(this.x1, this.y1, this.x2, this.y2);
+    }
 }
